70739c6e171d88e3e632611d7d22b2c6e22064f9
KJG04 <freedom7113@gmail.com>
2024-08-21 17:16:46 +0900

    초기 커밋

---
+++ src/test/java/com/sparta/spartaspringpracticeproject/SpartaSpringPracticeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartaspringpracticeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaSpringPracticeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
